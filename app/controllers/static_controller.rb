550089d393fd762890a218ad9bc6f6011b0816b2
Maria McLaughlin <mariamclaughlin@college.harvard.edu>
2016-04-11 21:48:37 -0400

    navbar working

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def devices
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/devices'
+
+  get 'static/help'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
