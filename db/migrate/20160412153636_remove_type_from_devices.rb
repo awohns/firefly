38f6dc726d639eea3ade644b815eacb30fd5d191
Wilder Wohns <awohns@gmail.com>
2016-04-19 19:44:49 -0400

    cleaning house

--- db/migrate/20160412153636_remove_type_from_devices.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromDevices < ActiveRecord::Migration
-  def change
-    remove_column :devices, :type, :string
-  end
-end
--- db/migrate/20160412153759_remove_errors_from_devices.rb
+++
@@ -1,5 +0,0 @@
-class RemoveErrorsFromDevices < ActiveRecord::Migration
-  def change
-    remove_column :devices, :errors, :text
-  end
-end
