cb29e5bc9346b74f6f0944abc6af34826959efd0
Wilder Wohns <awohns@gmail.com>
2016-04-23 17:03:54 -0400

    made javascript file for storage

---
+++ staticwebsitecode/HTML/js/hospitals.js
@@ -0,0 +1,14 @@
+//My variables
+var left = 0;
+var up = 150;
+var counter = 0;
+
+var storedHospitals = localStorage.getItem("hospitals");
+var hospitalsArray = JSON.parse(storedhospitals);
+var storedDevices = localStorage.getItem('devices');
+var devicesArray = JSON.parse(storedDevices);
+
+$(document).ready(function(){
+  
+
+  }
