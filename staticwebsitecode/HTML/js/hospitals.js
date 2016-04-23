67063c8c7182f6579fe1744c0c37a13d2a28e1d0
Wilder Wohns <awohns@gmail.com>
2016-04-23 17:30:27 -0400

    able to pass text to search button

--- staticwebsitecode/HTML/js/hospitals.js
+++ staticwebsitecode/HTML/js/hospitals.js
@@ -4,13 +4,16 @@ var up = 150;
 var counter = 0;
 
 var storedHospitals = localStorage.getItem("hospitals");
-var hospitalsArray = JSON.parse(storedhospitals);
+var hospitalsArray = JSON.parse(storedHospitals);
 var storedDevices = localStorage.getItem('devices');
 var devicesArray = JSON.parse(storedDevices);
 
 // $(document).ready(function(){
   
 function doSearch(s) {
-    console.log("hi")             // The function returns the product of p1 and p2
+    console.log("hi");
+    var searchedhospitals = s  ;   
+    console.log(searchedhospitals)    ;  
+    localStorage.setItem('hospitals', JSON.stringify(searchedhospitals));
 }
  // })
